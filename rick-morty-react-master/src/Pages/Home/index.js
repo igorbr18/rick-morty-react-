0cbfe4cdf58f2a38433c572b0ad4d5e8e3c54814
igorbr18 <92476751+igorbr18@users.noreply.github.com>
2021-11-18 07:34:55 -0400

    initial commit

---
+++ rick-morty-react-master/src/Pages/Home/index.js
@@ -0,0 +1,14 @@
+import HeaderResponsive from "../../components/HeaderResponsivo";
+import MainSection from "../../components/MainSection";
+
+function Home() {
+    return (
+        <div>
+        <HeaderResponsive />
+        <MainSection />
+        </div>
+    
+    );
+  }
+  
+  export default Home;
---
+++ rick-morty-react-master/src/Pages/Personagens/index.js
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import HeaderResponsive from "../../components/HeaderResponsivo";
+import api from "../../Api";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import './styles.css';
+import Card from "../../components/Card";
+
+function Personagens() {
+
+    const [data, setData] = useState(null);
+    const [name, setName] = useState("");
+
+    async function load() {
+        let res = await api.getPersonagemByName(name);
+        setData(res)
+
+    }
+
+    async function searchCharacter(event) {
+        event.preventDefault();
+        setData(null);
+        if (name) {
+            load();
+        }
+    }
+
+    function handleName(event) {
+        setName(event.target.value);
+
+
+    }
+
+    return (
+
+        <div>
+            <HeaderResponsive />
+            <div className="search-container">
+                <form onSubmit={searchCharacter}>
+                    <input onChange={handleName} />
+                    <button type="submit">
+                        <FontAwesomeIcon icon={faSearch} size="lg" />
+                    </button>
+                </form>
+            </div>
+            <div className="results-container">
+                {data &&
+
+                    ((data.error)
+                        ? <h1>Busca não encontrada</h1>
+                        : data.results.map((item, key) => {
+                            return <Card
+                                key={key}
+                                id={item.id}
+                                img={item.image}
+                                name={item.name}
+
+
+                            />
+
+                        })
+                    )
+                }
+            </div>
+        </div>
+
+    );
+}
+export default Personagens;
